7a28ba9788d570172c6d8260f645b013ef1bb046
Aleksey Burn <41241809+alekseyburn@users.noreply.github.com>
2018-09-24 16:45:20 +0300

    оживил модальное окно, сделал лого активным

--- js/script.js
+++ js/script.js
@@ -1,3 +1,67 @@
+var popup = document.querySelector(".write-us");
+var link = document.querySelector(".popup-open");
+var close = popup.querySelector(".modal-close");
+
+var form = popup.querySelector(".write-us-form");
+var username = popup.querySelector("[name=name]");
+var email = popup.querySelector("[name=e-mail]");
+var text = popup.querySelector("[name=text]");
+var isStorageSupport = true;
+var storage = {
+  username: "",
+  email: ""
+};
+
+try {
+  storage.username = localStorage.getItem("username");
+  storage.email = localStorage.getItem("email");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("write-us-on");
+
+  if (storage) {
+    username.value = storage.username;
+    email.value = storage.email;
+    text.focus();
+  } else {
+    username.focus();
+  }
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("write-us-on");
+  popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!username.value || !email.value || !text.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("username", username.value);
+      localStorage.setItem("email", email.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("write-us-on")) {
+      popup.classList.remove("write-us-on");
+      popup.classList.remove("modal-error");
+    }
+  }
+});
+
 var slider = document.querySelector(".features");
 var slider_slides = slider.querySelectorAll(".features-item");
 var slider_controls = slider.querySelectorAll(".slider-control");
